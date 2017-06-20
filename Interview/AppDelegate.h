7479d52feeed7f0d12c0ea78f78376c58a2f8332
Andrew Veresov <sou.git.sou@gmail.com>
2017-06-20 17:10:21 +0300

    init

---
+++ Interview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Interview
+//
+//  Created by Andrew Veresov on 6/20/17.
+//  Copyright © 2017 sou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
